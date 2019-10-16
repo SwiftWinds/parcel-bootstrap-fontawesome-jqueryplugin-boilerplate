4b0ef1ff7ea07d17b122bbc30dc9bed67eab43d0
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-15 19:14:36 -0700

    Comment out venobox and write instructions for jQuery plugin usage

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
 import "@fortawesome/fontawesome-free/css/all.css";
-import "venobox/venobox/venobox.min";
-import "venobox/venobox/venobox.css";
+// import "venobox/venobox/venobox"; <- example of jQuery plugin
+// import "venobox/venobox/venobox.css";
 
 $(document).ready(function() {
   $("#msgid").text("This is Hello World by JQuery");
-  $(".venobox").venobox();
+  // $(".venobox").venobox(); <- example of jQuery plugin usage
 });
